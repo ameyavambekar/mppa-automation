e4dae63c0a99de7a6233ef8d855b0898605bf208
Ameya Vambekar <ameyavambekar@users.noreply.github.com>
2026-05-26 00:38:25 +0530

    Added changed files and resolved conflicts

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -46,5 +46,3 @@ class DashboardPage(BasePage):
     def home_link(self):
         return self.page.get_by_role("link", name="← Home")
 
-    @allure.step("Login as {username}")
-    def login(self, username: str, password: str):
